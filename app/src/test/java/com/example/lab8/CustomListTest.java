154d236db85ff4b5b0491ff4c589ba715da37ceb
meishahuide <932776527@qq.com>
2023-03-07 17:53:29 -0700

    pass

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> citylist = new ArrayList<>();
 
+
+    @Test
+    public void testAddCity(){
+        CustomList list = new CustomList(null,new ArrayList<>());
+        City city = new City("Estevan","SK");
+        list.addCity(city);
+        assertEquals(1,list.getCount());
+    }
 
 }
